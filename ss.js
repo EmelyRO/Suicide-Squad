543f718ad1c317c87587afd02af5163a39f12df6
emely rosa <emely.rosa@student.uncommonhs.org>
2016-08-12 22:40:34 -0400

    add pause and stop buttons. stop button restarts song.made buttons smaller and moved them

--- ss.js
+++ ss.js
@@ -1,6 +1,15 @@
-function play(){
-       var audio = document.getElementById("audio");
-       audio.play();
-                 }
+var audio = document.getElementById("audio");
+function audioplay(){
+	audio.play();
+}
+
+function audiopause(){
+	audio.pause();
+}
+
+function audiostop(){
+	audio.pause();
+	audio.currentTime = 0;
+}
 
 /*allows the song to play when the button is pressed*/
